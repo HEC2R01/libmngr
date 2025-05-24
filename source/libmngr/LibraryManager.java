9144262b166310536370f703a5159dd0bebf25bf
unknown <hec2r01@gmail.com>
2025-05-24 15:21:06 +0900

    change the 'authorIndexer' field to a 'BSTree<String, Integer>' to compensate for the Author class no longer being comparable

--- source/libmngr/LibraryManager.java
+++ source/libmngr/LibraryManager.java
@@ -4,14 +4,14 @@ public class LibraryManager {
   // TODO: Look into 'B Trees' for indexing and 'B+ Trees' for database storage
   //       if we have enough time (or motivation...).
   private BSTree<String, Integer> titleIndexer;
-  private BSTree<Author, Integer> authorIndexer;
+  private BSTree<String, Integer> authorIndexer;
   private BSTree<Integer, Book> bookDatabase;
 
   public LibraryManager() {
     Logger.info("Creating 'LibraryManager' instance...");
   
     this.titleIndexer = new BSTree<String, Integer>();
-    this.authorIndexer = new BSTree<Author, Integer>();
+    this.authorIndexer = new BSTree<String, Integer>();
     this.bookDatabase = new BSTree<Integer, Book>();
   }
 
@@ -24,8 +24,21 @@ public class LibraryManager {
     Logger.info("===================================");
 
     this.titleIndexer.insert(book.getTitle(), book.getId());
-    this.authorIndexer.insert(book.getAuthor(), book.getId());
+    this.authorIndexer.insert(book.getAuthor().toString(), book.getId());
     this.bookDatabase.insert(book.getId(), book);
+  }
+
+  public void removeBook(Book book) {
+    Logger.info("========= [Removing Book] =========");
+    Logger.info("Title: %s", book.getTitle());
+    Logger.info("Author: %s", book.getAuthor().toString());
+    Logger.info("Genres: %s", book.getGenres().toString());
+    Logger.info("Id: %d", book.getId());
+    Logger.info("===================================");
+
+    this.titleIndexer.remove(book.getTitle());
+    this.authorIndexer.remove(book.getAuthor().toString());
+    this.bookDatabase.remove(book.getId());
   }
 
   public Book getBook(String title) {
@@ -38,7 +51,7 @@ public class LibraryManager {
   }
 
   public Book getBook(Author author) {
-    var n = authorIndexer.search(author);
+    var n = authorIndexer.search(author.toString());
     if (n == null) {
       return null;
     }
