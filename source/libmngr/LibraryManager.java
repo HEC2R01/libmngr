4f437fd206f6cd9f84495779261addb6aa1a47ab
unknown <hec2r01@gmail.com>
2025-05-24 16:21:59 +0900

    Reapply "remove 'bookDatabase' field from the LibraryManager class and changed the indexers to contain Book objects instead of indexes"
    This reverts commit d9ef73cb6eda9f8b80cb51607511ffd6ca90fb2d.

--- source/libmngr/LibraryManager.java
+++ source/libmngr/LibraryManager.java
@@ -3,16 +3,14 @@ package libmngr;
 public class LibraryManager {
   // TODO: Look into 'B Trees' for indexing and 'B+ Trees' for database storage
   //       if we have enough time (or motivation...).
-  private BSTree<String, Integer> titleIndexer;
-  private BSTree<String, Integer> authorIndexer;
-  private BSTree<Integer, Book> bookDatabase;
+  private BSTree<String, Book> titleIndexer;
+  private BSTree<String, Book> authorIndexer;
 
   public LibraryManager() {
     Logger.info("Creating 'LibraryManager' instance...");
   
-    this.titleIndexer = new BSTree<String, Integer>();
-    this.authorIndexer = new BSTree<String, Integer>();
-    this.bookDatabase = new BSTree<Integer, Book>();
+    this.titleIndexer = new BSTree<String, Book>();
+    this.authorIndexer = new BSTree<String, Book>();
   }
 
   public void addBook(Book book) {
@@ -23,9 +21,8 @@ public class LibraryManager {
     Logger.info("Id: %d", book.getId());
     Logger.info("===================================");
 
-    this.titleIndexer.insert(book.getTitle(), book.getId());
-    this.authorIndexer.insert(book.getAuthor().toString(), book.getId());
-    this.bookDatabase.insert(book.getId(), book);
+    this.titleIndexer.insert(book.getTitle(), book);
+    this.authorIndexer.insert(book.getAuthor().toString(), book);
   }
 
   public void removeBook(Book book) {
@@ -38,24 +35,15 @@ public class LibraryManager {
 
     this.titleIndexer.remove(book.getTitle());
     this.authorIndexer.remove(book.getAuthor().toString());
-    this.bookDatabase.remove(book.getId());
   }
 
   public Book getBook(String title) {
-    var n = titleIndexer.search(title);
-    if (n == null) {
-      return null;
-    }
-
-    return bookDatabase.search(n.getValue()).getValue();
+    var node = titleIndexer.search(title);
+    return node == null ? null : node.getValue();
   }
 
   public Book getBook(Author author) {
-    var n = authorIndexer.search(author.toString());
-    if (n == null) {
-      return null;
-    }
-
-    return bookDatabase.search(n.getValue()).getValue();
+    var node = authorIndexer.search(author.toString());
+    return node == null ? null : node.getValue();
   }
 }
