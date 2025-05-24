3b47fcb1d7e3ec2e256b34067235de24a7a36703
unknown <hec2r01@gmail.com>
2025-05-24 15:19:58 +0900

    remove 'Comparable<T>' interface from the Author class

--- source/libmngr/Author.java
+++ source/libmngr/Author.java
@@ -1,18 +1,11 @@
 package libmngr;
 
-import java.lang.Comparable;
-
-public class Author implements Comparable<Author> {
+public class Author {
   private String name;
 
   public Author(String name) {
     this.name = name;
   }
-
-  @Override
-  public int compareTo(Author o) {
-    return name.compareTo(o.name);
-  } 
 
   public String getName() { return name; }
   public Author setName(String name) { this.name = name; return this; }
